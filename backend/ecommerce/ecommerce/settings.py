f36721c7d63332a07ed9259ebd26255e99912b4b
wentao-g <wentao1695@gmail.com>
2026-07-20 02:01:06 +0800

    Setup jazzmin

--- backend/ecommerce/ecommerce/settings.py
+++ backend/ecommerce/ecommerce/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -115,3 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+JAZZMIN_SETTINGS = {
+    "site_title": "Ecommerce",
+    "site_header": "Ecommerce",
+    "site_brand": "Ecommerce",
+    "welcome_sign": "Welcome to Ecommerce",
+    "show_ui_builder": True
+}

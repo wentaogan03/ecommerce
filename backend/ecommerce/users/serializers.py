de3b3f3ab24be27ed1c1d3373f377e5b3934badc
wentao-g <wentao1695@gmail.com>
2026-07-21 15:01:56 +0800

    Create register serializer

--- backend/ecommerce/users/serializers.py
+++ backend/ecommerce/users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from users.models import User, Profile
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth.password_validation import validate_password
 
 class UserSerializer(serializers.ModelSerializer):
 
@@ -25,3 +26,26 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token['username'] = user.username
         return token
 
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'phone', 'password', 'password2')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            phone=validated_data['phone']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user

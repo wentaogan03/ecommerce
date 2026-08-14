fc7ad207570d34354d9ef1e51ff81db135d92f5b
wentao-g <wentao1695@gmail.com>
2026-08-15 02:54:02 +0800

    Create axios instance

---
+++ frontend/src/utils/axios.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import { BASE_URL } from './constant.js';
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  timeout: 5000,
+  headers: { "Content-Type": "application/json" },
+})
+
+export default api;

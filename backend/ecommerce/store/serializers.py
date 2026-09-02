e6d67a8dc417a625ee933fb484c3280573da53bc
wentaogan03 <wentaogan03@gmail.com>
2026-09-02 14:51:38 +0800

    Create serializers for related store models

---
+++ backend/ecommerce/store/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from store.models import Store, Product, Category
+from users.serializers import UserSerializer
+
+class StoreSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Store
+        fields = "__all__"
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    store = StoreSerializer()
+
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+class CategorySerializer(serializers.ModelSerializer):
+
+     class Meta:
+         model = Category
+         fields = "__all__"
